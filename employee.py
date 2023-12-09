b5e320e2d34fa40e6fa7821b9be2b77ea6d2f8c4
Megeran <leselam21@gmail.com>
2023-12-09 16:16:04 +0900

    Added output examples in Employee class

--- employee.py
+++ employee.py
@@ -7,14 +7,16 @@
 """
 
 class Employee:
+     count = 0
      def __init__(self, first_name, last_name, employee_id, salary):
          self.first_name = first_name
          self.last_name =  last_name
          self.employee_id = employee_id
          self.pay = salary
+         Employee.count +=1
      
      def get_full_name (self):
-        return "Employee Full Name : {}  {}".format(self.first_name, self.last_name)
+        return "{}  {}".format(self.first_name, self.last_name)
        
      def display_info(self):
          print(f"Employee_Id:{self.employe_id}, Name :{self.name},  salary: {self.salary:.3f}")
@@ -32,7 +34,16 @@ class Employee:
 
 if __name__ == '__main__':
     emp =  Employee("Selam", "Demise", "001", 5000)
+    emp2 =  Employee("Gebre", "Gemechu", "002", 7000)
+    emp3 =  Employee("Hagos", "Desta", "003", 6000)
+    emp4 =  Employee("Gemechu", "Demise", "004", 15000)
+
     print(f"1. {emp.get_full_name()}")
     emp.apply_raise(20)
     print(f"2. {emp.get_pay()}")
     print(f"3. {emp}")
+    print(f"4. {emp4}")
+    print(f"5. {emp.get_full_name()} said there are {emp.count} employees")
+    print(f"6. {emp4.get_full_name()} said there are {emp4.count} employees")
+    print(f"7. Woow! how {emp.get_full_name()} knew that there are {emp4.count} employees")
+    print(f"8. Did {emp.get_full_name()} know everything {emp4.get_full_name()} knows???")
