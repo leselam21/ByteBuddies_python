8caec569e39f65f0763e82b116c19c4bce9b4f5a
Megeran <leselam21@gmail.com>
2023-12-09 15:52:13 +0900

    added file employee.py

---
+++ employee.py
@@ -0,0 +1,16 @@
+"""
+  Inheritance :
+            Employee: Parent class 
+            Manager: Subclass 
+  Purpose: Demonstrating Inheritance relationship in Python
+  
+"""
+#Employee class
+
+class Employee:
+     pass
+
+
+#Manager Class 
+class Manager:
+     pass 
